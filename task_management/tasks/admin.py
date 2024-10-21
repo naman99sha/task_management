9af25eebff80b274867f6bf50aab73f473a655e4
Naman Sharma <naman99sha@gmail.com>
2024-10-21 21:45:21 +0530

    Modified admin.py to show IDs

--- task_management/tasks/admin.py
+++ task_management/tasks/admin.py
@@ -3,6 +3,9 @@ from .models import (
     Task,
     User
 )
+
+class CustomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
 # Register your models here.
-admin.site.register(Task)
-admin.site.register(User)
+admin.site.register(Task, CustomAdmin)
+admin.site.register(User, CustomAdmin)
